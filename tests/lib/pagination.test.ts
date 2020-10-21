54004739d1e2035d5b95e5b5de54d0233df1ecb1
Andy Taylor <a.taylor@kainos.com>
2020-10-21 17:46:34 +0000

    Bugfix/Pagination of final page

--- tests/lib/pagination.test.ts
+++ tests/lib/pagination.test.ts
@@ -11,6 +11,16 @@ describe('Test pagination', () => {
     expect(paginatedItems).toEqual(['6', '7', '8', '9', '10']);
   });
 
+  test('paginate() returns expected items when not enough items to fill last page', () => {
+    const sortedItems: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const page = '2';
+    const limit = '5';
+
+    const paginatedItems: string[] = pagination.paginate(sortedItems, page, limit);
+
+    expect(paginatedItems).toEqual(['6', '7', '8', '9']);
+  });
+
   test('paginate() returns expected items when page is undefined', () => {
     const sortedItems: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
     const page = undefined;
