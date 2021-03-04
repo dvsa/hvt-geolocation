8e48ab233efb5c497d28300ceac40cc8b4f665d1
David McKinney <d.mckinney@kainos.com>
2021-03-04 11:05:09 +0000

    BL11930 Fix bug in filtering by availability where end date must be parsed before being passed to date-fns function

--- src/lib/filterAtfs.ts
+++ src/lib/filterAtfs.ts
@@ -1,16 +1,16 @@
 import { utcToZonedTime } from 'date-fns-tz';
-import { isValid } from 'date-fns';
+import { isValid, parseISO } from 'date-fns';
 import { AuthorisedTestingFacility } from '../models/authorisedTestingFacility';
 
 // eslint-disable-next-line max-len
 const removeAtfsWithNoAvailability = (items: AuthorisedTestingFacility[]): AuthorisedTestingFacility[] => items.filter((item) => item?.availability?.isAvailable !== false
-        || !isAvailabilityInformationUpToDate(item?.availability?.endDate));
-
-const isAvailabilityInformationUpToDate = (date: string | undefined): boolean => date
-    && isValid(date) && !isDateBeforeToday(date);
+        || !isAvailabilityInformationUpToDate(parseISO(item?.availability?.endDate)));
 
 // eslint-disable-next-line max-len
-const isDateBeforeToday = (date: string): boolean => utcToZonedTime(new Date(date), process.env.TIMEZONE) < utcToZonedTime(new Date(), process.env.TIMEZONE);
+const isAvailabilityInformationUpToDate = (date: Date | undefined): boolean => (date ? isValid(date) && !isDateBeforeToday(date) : false);
+
+// eslint-disable-next-line max-len
+const isDateBeforeToday = (date: Date): boolean => utcToZonedTime(new Date(date), process.env.TIMEZONE) < utcToZonedTime(new Date(), process.env.TIMEZONE);
 
 export const filterAtfs = {
   removeAtfsWithNoAvailability,
--- tests/lib/filterAtfs.test.ts
+++ tests/lib/filterAtfs.test.ts
@@ -3,12 +3,14 @@ import { filterAtfs } from '../../src/lib/filterAtfs';
 
 describe('filterAtfs library unit tests', () => {
   describe('removeAtfsWithNoAvailability tests', () => {
-    const futureEndDate = new Date().setDate(new Date().getDate() + 5);
+    const futureEndDate = new Date();
+    futureEndDate.setDate(new Date().getDate() + 5);
+    const futureEndDateIsoString = futureEndDate.toISOString();
     test('ATFs with no information should not be removed from the list returned', () => {
       const atfs: AuthorisedTestingFacility[] = <AuthorisedTestingFacility[]><unknown>[
         { availability: { isAvailable: undefined, endDate: futureEndDate } },
-        { availability: { isAvailable: true, endDate: futureEndDate } },
-        { availability: { isAvailable: true, endDate: futureEndDate } },
+        { availability: { isAvailable: true, endDate: futureEndDateIsoString } },
+        { availability: { isAvailable: true, endDate: futureEndDateIsoString } },
       ];
       const result: AuthorisedTestingFacility[] = filterAtfs.removeAtfsWithNoAvailability(atfs);
       expect(result).toEqual(atfs);
@@ -16,22 +18,22 @@ describe('filterAtfs library unit tests', () => {
 
     test('ATFs with no availability should be removed from the list returned', () => {
       const atfs: AuthorisedTestingFacility[] = <AuthorisedTestingFacility[]><unknown>[
-        { availability: { isAvailable: false, endDate: futureEndDate } },
-        { availability: { isAvailable: true, endDate: futureEndDate } },
-        { availability: { isAvailable: false, endDate: futureEndDate } },
+        { availability: { isAvailable: false, endDate: futureEndDateIsoString } },
+        { availability: { isAvailable: true, endDate: futureEndDateIsoString } },
+        { availability: { isAvailable: false, endDate: futureEndDateIsoString } },
       ];
       const result: AuthorisedTestingFacility[] = filterAtfs.removeAtfsWithNoAvailability(atfs);
       const expectedAtfs: AuthorisedTestingFacility[] = <AuthorisedTestingFacility[]><unknown>[
-        { availability: { isAvailable: true, endDate: futureEndDate } },
+        { availability: { isAvailable: true, endDate: futureEndDateIsoString } },
       ];
       expect(result).toEqual(expectedAtfs);
     });
 
     test('ATFs with availability should not be removed from the list returned', () => {
       const atfs: AuthorisedTestingFacility[] = <AuthorisedTestingFacility[]><unknown>[
-        { availability: { isAvailable: true, endDate: futureEndDate } },
-        { availability: { isAvailable: true, endDate: futureEndDate } },
-        { availability: { isAvailable: true, endDate: futureEndDate } },
+        { availability: { isAvailable: true, endDate: futureEndDateIsoString } },
+        { availability: { isAvailable: true, endDate: futureEndDateIsoString } },
+        { availability: { isAvailable: true, endDate: futureEndDateIsoString } },
       ];
       const result: AuthorisedTestingFacility[] = filterAtfs.removeAtfsWithNoAvailability(atfs);
       expect(result).toEqual(atfs);
@@ -46,18 +48,19 @@ describe('filterAtfs library unit tests', () => {
     });
 
     test('ATFs with isAvailable set to false but with an end date in the past should not be removed', () => {
-      const yesterday = new Date().setDate(new Date().getDate() - 1);
+      const yesterday = new Date();
+      yesterday.setDate(new Date().getDate() - 1);
       const oneHourInMs = 1000 * 60 * 60;
-      const dateOneHourAgo = Date.now() - oneHourInMs;
+      const dateOneHourAgo = new Date(Date.now() - oneHourInMs).toISOString();
 
       const atfs: AuthorisedTestingFacility[] = <AuthorisedTestingFacility[]><unknown>[
-        { availability: { isAvailable: false, endDate: yesterday } },
+        { availability: { isAvailable: false, endDate: yesterday.toISOString() } },
         { availability: { isAvailable: false, endDate: dateOneHourAgo } },
-        { availability: { isAvailable: false, endDate: futureEndDate } },
+        { availability: { isAvailable: false, endDate: futureEndDateIsoString } },
       ];
       const result: AuthorisedTestingFacility[] = filterAtfs.removeAtfsWithNoAvailability(atfs);
       const expectedAtfs: AuthorisedTestingFacility[] = <AuthorisedTestingFacility[]><unknown>[
-        { availability: { isAvailable: false, endDate: yesterday } },
+        { availability: { isAvailable: false, endDate: yesterday.toISOString() } },
         { availability: { isAvailable: false, endDate: dateOneHourAgo } },
       ];
       expect(result).toEqual(expectedAtfs);
@@ -68,6 +71,7 @@ describe('filterAtfs library unit tests', () => {
         { availability: { isAvailable: false, endDate: 102029222222292e9 } },
         { availability: { isAvailable: false, endDate: '' } },
         { availability: { isAvailable: false, endDate: NaN } },
+        { availability: { isAvailable: false, endDate: undefined } },
       ];
       const result: AuthorisedTestingFacility[] = filterAtfs.removeAtfsWithNoAvailability(atfs);
       expect(result).toEqual(atfs);
