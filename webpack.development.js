da5fc0313156f5795ca800728219b0e017d55640
Anna Dodson <Anna.Dodson@DVSA.gov.uk>
2021-03-12 13:09:38 +0000

    Remove .env from prod build and optionally pass in commit hash
    - Can now optionally pass in `env.commit=<something>` when building in the pipeline
    so the zip file is named correctly through build config files, not through
    the Jenkinsfile scripts
    - Pipeline can now call build command:
    `npm run build:prod -- --env.commit=<commit>`
    - Also remove the .env file from prod builds as this file is only required
    for dev builds and should not be included
    - Ticket #BL-11943

--- webpack.development.js
+++ webpack.development.js
@@ -1,7 +1,16 @@
 const { merge } = require('webpack-merge');
+const CopyPlugin = require('copy-webpack-plugin');
 const common = require('./webpack.common.js');
+const lambdaName = 'GeolocationFunction'; // must correspond to lambda name in template.yml
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: './.env', to: `.aws-sam/build/${lambdaName}/` },
+      ],
+    }),
+  ],
 });
